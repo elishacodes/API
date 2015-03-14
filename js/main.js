db34dcba94ba5f656e80ed7524a32b6681b25b66
Zoe Ames <zoeames@gmail.com>
2015-03-14 15:07:48 -0500

    moonphase and lowest temp functions added

--- js/main.js
+++ js/main.js
@@ -3,14 +3,29 @@
 //What is the phase of the moon tonight?
 
 function moonPhase() {
-
+	$.ajax({
+		url: "http://api.wunderground.com/api/536b90dc843299c1/astronomy/q/37209.json",
+		dataType: "jsonp",
+		success : function(data){
+			console.log(data);
+			console.log("The moon phase is ", data.moon_phase.phaseofMoon);
+		}
+	});
 }
 moonPhase();
 
 
 //In what year was the record low temperature for today's date?  use almanac
 function recordLow() {
-
+  $.ajax({
+    url : "http://api.wunderground.com/api/536b90dc843299c1/almanac/q/CA/37209.json",
+    dataType : "jsonp",
+    success : function(data) {
+      console.log(data);
+      var year = data.almanac.temp_low.recordyear;
+      console.log("The lowest temp was in", year);
+      }
+    });
 }
 recordLow();
 
